1ed9c8ce1c59e45db98391b03b30d66270258caa
Marvin Tam <marvintam@gmail.com>
2015-10-11 18:53:21 -0400

    Use chai-jquery

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,8 @@ module.exports = function (config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/chai-jquery/chai-jquery.js',
       'angular-money-directive.js',
       'test/angular-money-directive.spec.js'
     ],
