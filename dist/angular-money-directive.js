5ddc304a4f8f918e411b97f64d57221d536bce4d
Marvin Tam <marvin@placemeter.com>
2015-12-14 13:03:43 -0500

    Build v1.2.5

--- dist/angular-money-directive.js
+++ dist/angular-money-directive.js
@@ -150,7 +150,7 @@ angular.module('fiestah.money', [])
     });
 
     ngModelCtrl.$formatters.push(function (value) {
-      if (value) {
+      if (isDefined(value)) {
         return isPrecisionValid() && isValueValid(value) ?
           formatPrecision(value) : value;
       } else {
